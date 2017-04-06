0bacdf885feeeb3f76f6d05157ddadd2153793e1
Prigya <prigya.shukla@gmail.com>
2017-04-06 11:50:22 +0530

    endpoint corrected

--- src/main/java/com/springboot/camel/CamelServiceRoute.java
+++ src/main/java/com/springboot/camel/CamelServiceRoute.java
@@ -13,7 +13,7 @@ public class CamelServiceRoute extends RouteBuilder{
 		from("timer:foo?fixedRate=true&period=5000&delay=3000")
 		//.to("restlet:http://localhost:8081/employees?restletMethod=get")
 		//.to("restlet:http://employeedetails01-lean-architecture-poc.44fs.preview.openshiftapps.com/employees?restletMethod=get")
-		.to("restlet:http://empdetailmaster-microservicesdemo.cloudapps.test.openshift.opentlc.com?restletMethod=get")
+		.to("restlet:http://empdetailmaster-microservicesdemo.cloudapps.test.openshift.opentlc.com/employees?restletMethod=get")
 			.process(new Processor(){
 				@Override
 		         public void process(Exchange exchange) throws Exception {
